6abd6b14b8ef30f082ae9026bafcf1f1ce7861b6
AlexeyLyndin <lyndin.alexey@gmail.com>
2016-06-28 21:44:01 +0300

    Initial commit.

---
+++ SystemTimeSetter/PrivelegeSetter/TimePrivilegeSetter.cs
@@ -0,0 +1,74 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace SystemTimeSetter.PrivelegeSetter
+{
+    internal static class TimePrivilegeSetter
+    {
+        public static bool SetSystemTimePrivilege()
+        {
+            return SetPrivilege("SeSystemtimePrivilege");
+        }
+
+        private static bool SetPrivilege(string privilege)
+        {
+            int i1 = 0;
+            var luid = new LUID();
+            var token_PRIVILEGES = new TOKEN_PRIVILEGES();
+            int i2 = OpenProcessToken(GetCurrentProcess(), 40, ref i1);
+            if (i2 == 0)
+            {
+                return false;
+            }
+
+            i2 = LookupPrivilegeValue(null, privilege, ref luid);
+            if (i2 == 0)
+            {
+                return false;
+            }
+
+            token_PRIVILEGES.PrivilegeCount = 1;
+            token_PRIVILEGES.Attributes = 2;
+            token_PRIVILEGES.Luid = luid;
+            i2 = AdjustTokenPrivileges(i1, 0, ref token_PRIVILEGES, 1024, 0, 0);
+            if (i2 == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        [PreserveSig]
+        [DllImport("advapi32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
+        private static extern int AdjustTokenPrivileges(int tokenhandle, int disableprivs,
+                                                        [MarshalAs(UnmanagedType.Struct)] ref TOKEN_PRIVILEGES newstate,
+                                        int bufferlength, int preivousState, int returnLength);
+
+        [PreserveSig]
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
+        private static extern int GetCurrentProcess();
+
+        [PreserveSig]
+        [DllImport("advapi32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
+        private static extern int LookupPrivilegeValue(string lpsystemname, string lpname,
+        [MarshalAs(UnmanagedType.Struct)] ref LUID lpLuid);
+
+        [PreserveSig]
+        [DllImport("advapi32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
+        private static extern int OpenProcessToken(int processHandle, int desiredAccess, ref int tokenhandle);
+
+        internal struct LUID
+        {
+            public int HighPart;
+            public int LowPart;
+        }
+
+        internal struct TOKEN_PRIVILEGES
+        {
+            public int Attributes;
+            public LUID Luid;
+            public int PrivilegeCount;
+        }
+    }
+}
---
+++ SystemTimeSetter/TimeSetter/TimeSetter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using SystemTimeSetter.PrivelegeSetter;
+
+namespace SystemTimeSetter.TimeSetter
+{
+    public static class TimeSetter
+    {
+        public static bool SetSystemTime(DateTime updateDateTime)
+        {
+            if (!SetTimePrivilege())
+            {
+                return false;
+            }
+
+            SystemTime systemTime = new SystemTime();
+            InitializeSystemTimeStructure(updateDateTime, ref systemTime);
+
+            bool setTimeResult = Win32SetSystemTime(ref systemTime);
+
+            return setTimeResult;
+        }
+
+        private static void InitializeSystemTimeStructure(DateTime updatedDateTime, ref SystemTime systemTime)
+        {
+            systemTime.Year = (ushort)updatedDateTime.Year;
+            systemTime.Month = (ushort)updatedDateTime.Month;
+            systemTime.Day = (ushort)updatedDateTime.Day;
+            systemTime.Hour = (ushort)updatedDateTime.Hour;
+            systemTime.Minute = (ushort)updatedDateTime.Minute;
+            systemTime.Second = (ushort)updatedDateTime.Second;
+            systemTime.Millisecond = (ushort)updatedDateTime.Millisecond;
+            systemTime.DayOfWeek = (ushort) updatedDateTime.DayOfWeek;
+        }
+
+        private static bool SetTimePrivilege()
+        {
+            return TimePrivilegeSetter.SetSystemTimePrivilege();
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "GetSystemTime", SetLastError = true)]
+        private extern static void Win32GetSystemTime(ref SystemTime sysTime);
+
+        [DllImport("kernel32.dll", EntryPoint = "SetSystemTime", SetLastError = true)]
+        private extern static bool Win32SetSystemTime(ref SystemTime sysTime);
+
+        private struct SystemTime
+        {
+            public ushort Year;
+            public ushort Month;
+            public ushort DayOfWeek;
+            public ushort Day;
+            public ushort Hour;
+            public ushort Minute;
+            public ushort Second;
+            public ushort Millisecond;
+        };
+    }
+}
